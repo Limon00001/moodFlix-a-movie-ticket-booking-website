434eb3d09198fda10384e2a27ca4b4fb63352b4a
Limon00001 <limonhossain82@gmail.com>
2025-07-02 22:47:31 +0600

    feat(client-project): implement routing and layout for movie ticket booking application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,47 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+// Internal Imports
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Favorite from './pages/Favorite';
+import Home from './pages/Home';
+import MovieDetails from './pages/MovieDetails';
+import Movies from './pages/Movies';
+import MyBookings from './pages/MyBookings';
+import SeatLayout from './pages/SeatLayout';
+
 // App Component
 const App = () => {
+  // Check if the current route is an admin route
+  const isAdminRoute = useLocation().pathname.startsWith('/admin');
+
+  // Render the application with routes
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster position="top-right" />
+      {/* Render Navbar only for non-admin routes */}
+      {!isAdminRoute && <Navbar />}
+
+      {/* Application routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+
+      {/* Render Footer only for non-admin routes */}
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
 
 // Export
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 02 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
 
+// External Imports
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Internal Imports
+import App from './App.jsx';
+
+// Styles
+import './index.css';
+
+// Render the App component into the root element
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>,
-)
+);
