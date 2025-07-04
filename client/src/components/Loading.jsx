b350ac23148019d19a97a51277e273a208bae3d1
Limon00001 <limonhossain82@gmail.com>
2025-07-04 16:14:59 +0600

    feat(client-loading): implement loading component for better user experience

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,18 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 04 Jul, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// Loading Component
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-[80vh]">
+      <div className="animate-spin rounded-full h-14 w-14 border-2 border-t-primary"></div>
+    </div>
+  );
+};
+
+// Export
+export default Loading;
