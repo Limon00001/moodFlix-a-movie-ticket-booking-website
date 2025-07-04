c8a228f597998afb51f2075c97169fa93d924ea3
Limon00001 <limonhossain82@gmail.com>
2025-07-04 10:24:54 +0600

    feat(client-movies): implement movies page with movie display

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -5,9 +5,36 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// Internal Imports
+import { dummyShowsData } from '../assets/assets';
+import BlurCircle from '../components/BlurCircle';
+import MovieCard from '../components/MovieCard';
+
 // All Movies Page Component for Movie Ticket Booking Application
 const Movies = () => {
-  return <div>Movies</div>;
+  // Check if there are any movies to display
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
+      {/* Blur Effect */}
+      <BlurCircle top="150px" left="0" />
+      <BlurCircle bottom="50px" right="50px" />
+
+      {/* Heading */}
+      <h1 className="font-medium text-lg my-4">Now Showing</h1>
+
+      {/* Movies Section */}
+      <div className="flex flex-wrap gap-8 max-sm:justify-center">
+        {dummyShowsData.map((movie) => (
+          <MovieCard key={movie._id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    // No Movies Found Section
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-center">No Movies Found</h1>
+    </div>
+  );
 };
 
 // Export
