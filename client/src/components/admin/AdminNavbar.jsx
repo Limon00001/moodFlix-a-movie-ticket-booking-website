2a1eb14b65479740bd805a6583501bc71480c178
Limon00001 <limonhossain82@gmail.com>
2025-07-06 22:35:29 +0600

    feat(client(admin)-navbar): implement navbar for admin

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -5,9 +5,23 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { Clapperboard } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
 // Navbar Component for Admin Page
 const AdminNavbar = () => {
-  return <div>AdminNavbar</div>;
+  return (
+    <div className="flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30">
+      {/* Logo */}
+      <Link to="/" className="flex items-center gap-2">
+        <Clapperboard className="w-9 h-9" />
+        <span className="text-2xl font-bold italic">
+          Mood<span className="text-primary">Flix</span>
+        </span>
+      </Link>
+    </div>
+  );
 };
 
 // Export
